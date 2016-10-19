e7e92051e2d764c701761fd059b662ad567cd7df
Yury <yury.skaletskiy@gmail.com>
2016-10-19 21:04:40 +0400

    initial revision

---
+++ Sw.EazyCounters.Tests/TestCounters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sw.EazyCounters.Tests
+{
+    [TestFixture]
+    public class TestCounters
+    {
+        [Test]
+        public void TestStore()
+        {
+            var cs = new CounterStore();
+            var c1 = cs.GetCounter("c1");
+            var c2 = cs.GetCounter("c2");
+
+            Assert.AreEqual(0, c1.TotalEvents);
+            Assert.AreEqual(0, c2.TotalEvents);
+
+            c1.RegisterEvent();
+            c1.RegisterEvent();
+            c2.RegisterEvent();
+
+            Assert.AreEqual(2, c1.TotalEvents);
+
+            Assert.AreEqual(1 / 60, c1.AvgEventsPerMinute);
+
+
+            Assert.AreEqual(1/24, c1.AvgEventsPerHour);
+
+            Assert.AreEqual(1, c2.TotalEvents);
+        }
+    }
+}
